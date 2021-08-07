db1e243a2da23d21ce9de0c8f888d49f7bea937a
cosmin.pr <cosmin.p@webwavers.net>
2021-08-08 00:28:45 +0300

    refactored to class components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,28 +9,61 @@ export default class Contact extends React.Component {
             message: "",
         }
     }
-    render () {
-        // const [name, setName] = React.useState("");
-        // const [email, setEmail] = React.useState("");
-        // const [message, setMessage] = React.useState("");
 
-        function encode(data) {
-            return Object.keys(data).map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])).join("&");
+    handleOnChange(e) {
+        if (e.target.id === "name") {
+            let nameCopy = this.state.name;
+            nameCopy = e.target.value;
+    
+            this.setState({
+                name: nameCopy,
+            });
         }
 
-        // function handleSubmit(e) {
-        //     e.preventDefault();
-        //     console.log("ENCODE", encode({"form-name" : "contact", name, email, message}));
+        if (e.target.id === "email") {
+            let emailCopy = this.state.email;
+            emailCopy = e.target.value;
+    
+            this.setState({
+                email: emailCopy,
+            });
+        }
 
-        //     fetch("/", {
-        //         method: "POST",
-        //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        //         body: encode({"form-name" : "contact", name, email, message})
-        //     })
-        //     .then(() => alert("Message sent"))
-        //     .catch((error) => alert(error));
-        // }
+        if (e.target.id === "message") {
+            let messageCopy = this.state.message;
+            messageCopy = e.target.value;
+    
+            this.setState({
+                message: messageCopy,
+            });
+        }
+    }
 
+    encode(data) {
+        return Object.keys(data).map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])).join("&");
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let name = this.state.name;
+        let email = this.state.email;
+        let message = this.state.message;
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: this.encode({
+                "form-name" : "contact",
+                name,
+                email,
+                message,
+            })
+        })
+        .then(() => alert("Message sent"))
+        .catch((error) => alert(error));
+    }
+
+    render () {
         return (
             <section id="contact" className="relative">
                 <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -38,7 +71,7 @@ export default class Contact extends React.Component {
                         className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
                         <iframe width="100%" height="100%" title="map" className="absolute inset-0" frameBorder={0} marginHeight={0}
                             marginWidth={0} style={{ filter: "opacity(0.7)" }}
-                            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" />
+                            src="https://maps.google.com/maps?q=cluj&t=&z=11&ie=UTF8&iwloc=&output=embed" />
                         <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
                             <div className="lg:w-1/2 px-6">
                                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
@@ -63,7 +96,7 @@ export default class Contact extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <form netlify name="contact" className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <form netlify name="contact" className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0" onSubmit={(e) => this.handleSubmit(e)}>
                         <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                             Hire Me
                         </h2>
@@ -75,19 +108,19 @@ export default class Contact extends React.Component {
                             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
                                 Name
                             </label>
-                            <input type="text" id="name" name="name" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={this.state.name}/>
+                            <input type="text" id="name" name="name" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => this.handleOnChange(e)}/>
                         </div>
                         <div className="relative mb-4">
                             <label htmlFor="email" className="leading-7 text-sm text-gray-400">
                                 Email
                             </label>
-                            <input type="email" id="email" name="email" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                            <input type="email" id="email" name="email" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => this.handleOnChange(e)}/>
                         </div>
                         <div className="relative mb-4">
                             <label htmlFor="message" className="leading-7 text-sm text-gray-400">
                                 Message
                             </label>
-                            <textarea id="message" name="message" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"/>
+                            <textarea id="message" name="message" className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" onChange={(e) => this.handleOnChange(e)}/>
                         </div>
                         <button type="submit"
                             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
