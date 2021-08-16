03beb6ab41e74bf148f5f94334e3e227d825f80e
cosmin.pr <cosmin.p@webwavers.net>
2021-08-16 15:25:12 +0300

    projects styling WIP

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,7 +11,7 @@ export default class Projects extends React.Component {
                     <div className={styles["description-wrapper"]}>
                         <i className={`fas fa-code fa-3x ${styles["code-icon"]}`}></i>
                         <h1 className={styles["title"]}>
-                            Apps I've Built
+                            <span>Apps I've Built</span>
                         </h1>
                         <p className={styles["description"]}>
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
@@ -19,24 +19,23 @@ export default class Projects extends React.Component {
                             fuga dolore.
                         </p>
                     </div>
-                    <div className="flex flex-wrap -m-4">
+                    <div className={styles["projects-wrapper"]}>
                         {projects.map((project) => (
-                        <a href={project.link} key={project.image} className="sm:w-1/2 w-100 p-4">
-                            <div className="flex relative">
-                                <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center"
-                                    src={project.image} />
-                                <div
-                                    className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                                    <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                                        {project.subtitle}
-                                    </h2>
-                                    <h1 className="title-font text-lg font-medium text-white mb-3">
-                                        {project.title}
-                                    </h1>
-                                    <p className="leading-relaxed">{project.description}</p>
+                            <a href={project.link} key={project.image} className={styles["project"]}>
+                                <div className={styles["project-wrapper"]}>
+                                    <img alt="gallery" className={styles["image"]}
+                                        src={project.image} />
+                                    <div className={styles["project-description"]}>
+                                        <h2 className={styles["project-title"]}>
+                                            {project.subtitle}
+                                        </h2>
+                                        <h3 className={styles["project-subtitle"]}>
+                                            {project.title}
+                                        </h3>
+                                        <p className={styles["project-details"]}>{project.description}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
+                            </a>
                         ))}
                     </div>
                 </div>
