241e5deda992cd608d9ebd02cbb5b398c34ea558
Cosmin Pruncu <cosminp@intranet.webwavers.net>
2021-08-20 09:54:47 +0300

    Testimonials WIP

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 import { testimonials } from "../../data";
 import styles from "./Testimonials.module.scss"
 
@@ -7,19 +6,19 @@ import styles from "./Testimonials.module.scss"
 export default class Testimonials extends React.Component {
     render () {
         return (
-            <section id="testimonials">
-                <div className="container px-5 py-10 mx-auto text-center">
-                    {/* <UsersIcon className="w-10 inline-block mb-4" /> */}
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+            <section id="testimonials" className={`container ${styles["testimonials"]}`}>
+                <div className={styles["description-wrapper"]}>
+                    <i className={`fas fa-user-edit fa-3x ${styles["people-icon"]}`}></i>
+                    <h1 className={styles["title"]}>
                         Client Testimonials
                     </h1>
-                    <div className="flex flex-wrap m-4">
+                    <div className={styles["testimonials"]}>
                         {testimonials.map((testimonial) => (
-                        <div className="p-4 md:w-1/2 w-full">
-                            <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                                {/* <TerminalIcon className="block w-8 text-gray-500 mb-4" /> */}
-                                <p className="leading-relaxed mb-6">{testimonial.quote}</p>
-                                <div className="inline-flex items-center">
+                        <div className={styles["testimony-wrapper"]}>
+                            <div className={styles["testimony-description-wrapper"]}>
+                                <i className={`fas fa-terminal ${styles["terminal-icon"]}`}></i>
+                                <p className={styles["description"]}>{testimonial.quote}</p>
+                                <div className={styles["image-wrapper"]}>
                                     <img alt="testimonial" src={testimonial.image}
                                         className="w-12 rounded-full flex-shrink-0 object-cover object-center" />
                                     <span className="flex-grow flex flex-col pl-4">
